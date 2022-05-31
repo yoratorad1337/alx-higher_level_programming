8f7e28f2325a761902aa65f32d4828337a0e60e4
yoratorad1337 <yoratorad0@gmail.com>
2022-05-31 23:56:42 +0300

    alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(97,123): #97 = 'a' and 122 = 'z'
-    print(f"{chr(i)}, end="") # chr(i)- convert int to char
+    print(f"{}".format(chr(i)), end="") # chr(i)- convert int to char
