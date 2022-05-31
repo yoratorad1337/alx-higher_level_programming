9a2778193fb6ae2c24738023fd3470a435c3c6a4
yoratorad1337 <yoratorad0@gmail.com>
2022-05-31 17:07:34 +0300

    pos | neg | zero

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if (number != 0 and number > 0):
+	print(f"{number}: is Positive")
+elif (number != 0 and number < 0):
+	printf(f"{number}: is Negative")
+else:
+	print(f"{number}: is Zero")
--- 0x01-python-if_else_loops_functions/0-postive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if (number == 0):
-	print(f"{number}: is Zero")
-elif (number > 0);
-	print(f"{number}: is Postive")
-else:
-	print(f"{number}: is Negative")
