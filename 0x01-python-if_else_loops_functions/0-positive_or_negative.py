141f08ea050c408b8d826c3d87e49e77dcf9859d
yoratorad1337 <yoratorad0@gmail.com>
2022-05-31 17:21:38 +0300

    nested if

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if (number > 0):
-	print(f"{number}: is Positive")
-elif (number < 0):
-	print(f"{number}: is Negative")
+if (number > 0 or number < 0):
+	if (number > 0):
+		print(f"{number}: is Positive")
+	else:
+		print(f"{number}: is Negative")
 else:
 	print(f"{number}: is Zero")
