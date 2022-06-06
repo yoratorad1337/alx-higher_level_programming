7e9255f30aa63f673d34ead71c6dd7508aa67e0a
yoratorad1337 <yoratorad0@gmail.com>
2022-06-06 19:41:10 +0300

    secure access

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if (-idx and idx > len(mylist)):
+    if (-id and idx > len(mylist)):
         return NONE
     else:
-        x = print("Element at index " + my_list[:] + "is " + my_list[idx])
-        return my_list[x]
+        return idx
