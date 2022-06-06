fc40dab591013ca4439ee3056c4a2d0b9bcd0e8a
yoratorad1337 <yoratorad0@gmail.com>
2022-06-06 19:27:52 +0300

    secure access to element in a list

---
+++ 0x03-python_data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if (-idx and idx > len(mylist)):
+        return NONE
+    else:
+    x = print("Element at index " + my_list[:] + "is " + my_list[idx])
+    return my_list[x]
